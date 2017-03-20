4154ac3ef7ef6512bc3b16cbe564cd73813a9ad5
Santiago Castro <santiago@xmartlabs.com>
2017-03-20 11:34:56 -0300

    Support spaces in APK path

--- decompileAPK.sh
+++ decompileAPK.sh
@@ -57,7 +57,7 @@ done
 apkfile=$1
 
 # Check a the minimum set of parameters is present. If not show help...
-if [ -z $apkfile ]
+if [ -z "$apkfile" ]
 then
   help
   exit
@@ -72,29 +72,29 @@ else
 fi
 resOutputDir="$outputDir/res-output"
 
-echo "Decompiling APK file $apkfile"
-echo "Results will be put in $outputDir"
+echo "Decompiling APK file \"$apkfile\""
+echo "Results will be put in \"$outputDir\""
 echo ""
 
 # Cleanup the output directories
 echo "Cleaning up the output directories"
-rm -Rf $DOE/output
-rm -Rf $DOE/output-res
-rm -Rf $outputDir
-mkdir -p $outputDir
+rm -Rf "$DOE/output"
+rm -Rf "$DOE/output-res"
+rm -Rf "$outputDir"
+mkdir -p "$outputDir"
 
 # Generate directories for project structure
 if [[ "$generateProject" == true ]];
 then
 	moduleName=app
 
-	mkdir -p $outputDir/gradle
-	mkdir -p $outputDir/gradle/wrapper
-	mkdir -p $outputDir/$moduleName
-	mkdir -p $outputDir/$moduleName/libs
-	mkdir -p $outputDir/$moduleName/src
-	mkdir -p $outputDir/$moduleName/src/main
-	mkdir -p $outputDir/$moduleName/src/main/java
+	mkdir -p "$outputDir/gradle"
+	mkdir -p "$outputDir/gradle/wrapper"
+	mkdir -p "$outputDir/$moduleName"
+	mkdir -p "$outputDir/$moduleName/libs"
+	mkdir -p "$outputDir/$moduleName/src"
+	mkdir -p "$outputDir/$moduleName/src/main"
+	mkdir -p "$outputDir/$moduleName/src/main/java"
 
 	baseOutputDir="$outputDir"
 	resOutputDir="$outputDir/$moduleName/src/main/res-output"
@@ -105,20 +105,20 @@ if [[ "$skipJava" == false ]];
 then
 	# Create JAR from APK file, then decompile that JAR to have Java files
 	echo "Extracting JAR file from APK"
-	sh $DOE/dex2jar/d2j-dex2jar.sh -o $outputDir/output.jar $apkfile
+	sh "$DOE/dex2jar/d2j-dex2jar.sh" -o "$outputDir/output.jar" "$apkfile"
 	echo "Decompiling JAR for Java files"
 	if [[ "$generateProject" == true ]];
 	then
-		java -jar $DOE/jd-core-java/jd-core-java-1.2.jar $outputDir/output.jar $outputDir
+		java -jar "$DOE/jd-core-java/jd-core-java-1.2.jar" "$outputDir/output.jar" "$outputDir"
 	else
-		java -jar $DOE/jd-core-java/jd-core-java-1.2.jar $outputDir/output.jar $outputDir/src
+		java -jar "$DOE/jd-core-java/jd-core-java-1.2.jar" "$outputDir/output.jar" "$outputDir/src"
 	fi
-	rm $outputDir/output.jar
+	rm "$outputDir/output.jar"
 
 	if [[ "$formatJava" == true ]];
 	then
 		echo "Start formatting all Java files"
-		$DOE/astyle/build/mac/bin/astyle -r -n -q --style=java -s4 -xc -S -K -j $outputDir/*.java
+		"$DOE/astyle/build/mac/bin/astyle" -r -n -q --style=java -s4 -xc -S -K -j "$outputDir"/*.java
 	fi
 else
 	echo "Skipping decompilation of JAVA files"
@@ -128,19 +128,19 @@ if [[ "$skipResources" == false ]];
 then
 	# Extract all resources from the APK and remove all the unnecessary files from the output
 	echo "Extracting resources from APK file"
-	java -jar $DOE/apktool/apktool.jar decode -f $apkfile $resOutputDir
-	rm -Rf $resOutputDir/smali
-	rm $resOutputDir/apktool.yml
+	java -jar "$DOE/apktool/apktool.jar" decode -f "$apkfile" "$resOutputDir"
+	rm -Rf "$resOutputDir/smali"
+	rm "$resOutputDir/apktool.yml"
 
 	if [[ "$generateProject" == true ]];
 	then
 		# Move the resource-output to correct project directory
-		mv $resOutputDir/* $baseOutputDir/$moduleName/src/main/
+		mv "$resOutputDir"/* "$baseOutputDir/$moduleName/src/main/"
 	else
 		# Move the resource-output to output directory
-		mv $resOutputDir/* $outputDir
+		mv "$resOutputDir"/* "$outputDir"
 	fi
-	rm -Rf $resOutputDir
+	rm -Rf "$resOutputDir"
 else
 	echo "Skipping decompilation of resources"
 fi
@@ -148,9 +148,9 @@ fi
 if [[ "$generateProject" == true ]];
 then
 	echo "Copying all Gradle project files in the decompiled project"
-	cp $DOE/files/project/* $baseOutputDir/
-	cp $DOE/files/wrapper/* $baseOutputDir/gradle/wrapper
-	cp $DOE/files/module/* $baseOutputDir/$moduleName
+	cp "$DOE/files/project"/* "$baseOutputDir/"
+	cp "$DOE/files/wrapper"/* "$baseOutputDir/gradle/wrapper"
+	cp "$DOE/files/module"/* "$baseOutputDir/$moduleName"
 fi
 
 exit;
